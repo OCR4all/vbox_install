f1019bd54d42d25944169aa9f764e2001375b77d
Yannik Herbst <yannik.herbst@stud-mail.uni-wuerzburg.de>
2019-11-06 11:49:22 +0100

    Update ocr4all_install.sh

--- ocr4all_install.sh
+++ ocr4all_install.sh
@@ -2,7 +2,8 @@
 
 su
 # Install dependencies system-wide and delete cache, removed supervisor docker dependency
-apt-get update && apt get install -y \
+apt-get update
+apt get install -y \
 locales git wget openjdk-8-jdk-headless tomcat8 python2.7 python3-pip \
 python3-pil maven python-tk python-pip
 
