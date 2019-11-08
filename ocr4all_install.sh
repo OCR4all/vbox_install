e4fc89ec7288a1866f5b1a4bea4d8b821ad7b3ba
Yannik Herbst <yannik.herbst@stud-mail.uni-wuerzburg.de>
2019-11-08 16:45:51 +0100

    Update ocr4all_install.sh

--- ocr4all_install.sh
+++ ocr4all_install.sh
@@ -15,7 +15,7 @@ echo "LANG=en_US.UTF-8" >> $ENVVAR
 echo "LANGUAGE=\"en_US:en\"" >> $ENVVAR
 echo "LC_ALL=\"en_US.UTF-8\"" >> $ENVVAR
 echo "CATALINA_HOME=\"/usr/share/tomcat8\"" >> $ENVVAR
-
+source /etc/environment.txt
 
 #Force Tomcat to use Java 8
         rm /usr/lib/jvm/default-java && \
