03365db199bbb5b2edecaa588c352be7bd71c20f
maxnth <maximilian.noeth@protonmail.com>
2020-11-25 18:41:45 +0100

    update config script

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ if [ ! -d "/etc/systemd/system/getty@tty1.service.d" ]
 then
   mkdir -p "/etc/systemd/system/getty@tty1.service.d"
 fi
-echo -e "[Service]\nExecStart=\nExecStart=-/sbin/agetty --autologin ocr4all --noclear %I $TERM\nType=idle" | tee /etc/systemd/system/getty@tty1.service.d/override.conf
+echo "[Service]\nExecStart=\nExecStart=-/sbin/agetty --autologin ocr4all --noclear %I $TERM\nType=idle" | tee /etc/systemd/system/getty@tty1.service.d/override.conf
 
 # Hide Ubuntu Server MOTD
 touch ~/.hushlogin
@@ -12,10 +12,7 @@ touch ~/.hushlogin
 # Hide cloud-init message output
 touch /etc/cloud/cloud-init.disabled
 
-if [ ! groups ocr4all | grep -q '\bvboxsf\b' ]
-then
-  usermod -a -G vboxsf ocr4all
-fi
+usermod -a -G vboxsf ocr4all
 
 BASE_DIR="/home/ocr4all/ocr4all"
 
@@ -23,3 +20,5 @@ if [ ! -d ${BASE_DIR} ]
 then
   mkdir -p ${BASE_DIR}
 fi
+
+reboot
