35846f9b150366409c50ba5f42622fd5fb544866
maxnth <maximilian.noeth@protonmail.com>
2020-11-27 13:34:14 +0100

    split docker run into docker create and docker start

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 BASE_DIR="/home/ocr4all/ocr4all"
+CONTAINER_NAME="ocr4all"
 
-docker run \
+docker create \
     -p 8080:8080 \
     -u `id -u root`:`id -g $USER` \
-    --name ocr4all \
+    --name ${CONTAINER_NAME} \
     -v ${BASE_DIR}/data:/var/ocr4all/data \
     -v ${BASE_DIR}/models:/var/ocr4all/models/custom \
     --restart always \
     -it ls6uniwue/ocr4all
+
+docker start ${CONTAINER_NAME}
